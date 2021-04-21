26b35b4e33ae3163e90a183887af311a61479eaa
Waranyu Khongchai <waranyu.khongchai@clbs.co.th>
2021-04-21 16:46:17 +0700

    CREATE xib subcell of setting

---
+++ DemoExpandCell.2/CellSetting/subTopInfoCell.swift
@@ -0,0 +1,23 @@
+//
+//  subTopInfoCell.swift
+//  DemoExpandCell.2
+//
+//  Created by PHENIXCRIME on 21/4/2564 BE.
+//
+
+import UIKit
+
+class subTopInfoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
